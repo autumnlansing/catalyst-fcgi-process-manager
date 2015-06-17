5a994d9c5bb8c79fee485d8f1dc29e6acce36954
Autumn Lansing <autumn@autumnlansing.com>
2015-06-17 14:15:37 -0500

    Added POD

--- fcgi_manager.pl
+++ fcgi_manager.pl
@@ -3,6 +3,33 @@
 use strict;
 use warnings;
 
+=head1 NAME
+
+Catalyst FCGI Process Monitor
+
+=head1 VERSION
+
+version 1.0
+
+=head1 SYNOPSIS
+
+Supply an argument for the maximum size of memory in kilobytes allowed each
+child process.
+
+    fcgi_manager.pl 300000
+
+=head1 DESCRIPTION
+
+Child processes created by a daemonized Catalyst myapp_fastcgi.pl script can
+sometimes grow significantly in memory size, causing the need to kill them
+from time to time or face server slowdown. Some of this may be due to memory
+leaks in your app, though some of it could be due to general copy-on-write
+behavior. To fix the latter, or to stave off fixing the former if you don't
+currently have time for the minutae of leak tracing, this script monitors
+fastcgi child processes and gracefully kills those who grow beyond a set size.
+
+=cut
+
 my ($kbytes) = @ARGV;
 
 die "Usage: fcgi_manager ram_limit_in_kilobytes\n" unless $kbytes;
@@ -20,3 +47,16 @@ foreach my $pid (@pids) {
         sleep 10;
     }
 }
+
+=head1 AUTHOR
+
+Autumn Lansing
+
+=head1 COPYRIGHT AND LICENSE
+
+This script is copyright (c) 2015 by Autumn Lansing.
+
+This is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
